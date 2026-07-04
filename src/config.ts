a1ff2cf3f589d2ffd29367d36e5f78138b02f4f9
Nguyenvudemo <nguyenphongvu86786@gmail.com>
2026-07-04 17:22:54 +0700

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,4 +4,4 @@
 
 // Hãy dán đường dẫn Web App URL lấy từ Google Apps Script (bước triển khai) vào đây.
 // Ví dụ: "https://script.google.com/macros/s/AKfycb.../exec"
-export const GOOGLE_SHEET_WEBHOOK_URL = "ĐƯỜNG_DẪN_WEB_APP_URL_CỦA_BẠN";
+export const GOOGLE_SHEET_WEBHOOK_URL = "https://script.google.com/macros/s/AKfycbzoGhT92wvJJNFJSH2cn1MmEji-980Q-EElYj1Iu8AbZJCU68rwcS2LYocbgVR5ORQ/exec";
